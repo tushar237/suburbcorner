9f4bf1886fc505a0464d4fdfa2dab22b9619fb49
tushar237 <tushargupta237@gmail.com>
2019-11-22 22:14:09 +0530

    Initial commit

---
+++ src/main/java/com/suburb/corner/SuburbCornerApplication.java
@@ -0,0 +1,13 @@
+package com.suburb.corner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuburbCornerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuburbCornerApplication.class, args);
+	}
+
+}
